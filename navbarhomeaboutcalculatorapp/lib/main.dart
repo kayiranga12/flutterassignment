a137531558583f87c98af1e9520824f914017520
Ornella <ornellakankazi@gmail.com>
2024-03-11 16:02:45 +0200

    Merge branch 'main' of https://github.com/kayiranga12/flutterassignment

--- navbarhomeaboutcalculatorapp/lib/main.dart
+++ navbarhomeaboutcalculatorapp/lib/main.dart
@@ -1,4 +1,7 @@
-
+import 'package:flutter/material.dart';
+import 'package:navbarhomeaboutcalculatorapp/Provider/provider.dart';
+import 'package:navbarhomeaboutcalculatorapp/pages/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
 void main() async {
